660c860209d36593d53102faf5e30428e7194c04
Philipe Moreira <xphilipsilva@gmail.com>
2026-09-10 16:28:28 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
 // dos arquivos principais.
 // ==========================================================================
 
-const NOME_DO_CACHE = "financas-familia-v90";
+const NOME_DO_CACHE = "financas-familia-v91";
 
 // Arquivos essenciais, guardados localmente no primeiro acesso
 const ARQUIVOS_ESSENCIAIS = [
